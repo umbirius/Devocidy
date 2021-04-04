c436735a8890fb69448d719b950cf75f55588b0b
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-04 10:00:21 -0400

    created log in page

---
+++ components/LogInSignUp/main-form.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import { CenterFocusStrong } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(3),
+  },
+  form: {
+    display: "flex",
+    position: "fixed",
+    top: "40%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    width: "400px",
+    padding: "20px",
+    alignItems: "center"
+  },
+  textField: {
+    width: "300px",
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+    width: "200px",
+    padding: "10px",
+    margin: "20px",
+  },
+}));
+
+
+
+const MainForm = () => {
+  const classes = useStyles();
+  return (
+      <Paper className={`${classes.root} ${classes.form}`}>
+        <form>
+          <TextField label="Username" variant="outlined"></TextField>
+          <br></br>
+          <TextField
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+          ></TextField>
+        </form>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          size="medium"
+          color="primary"
+        >
+          Log In
+        </Button>
+      </Paper>
+  );
+};
+
+export default MainForm;
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import MainForm from '@/components/LogInSignUp/main-form'
+
+
 
 const Login = () => {
   return (
-    <div>
-      Login <br />
-      or SignUp
-    </div>
+    // <div>
+    //   Login <br />
+    //   or SignUp
+    // </div>
+   <MainForm></MainForm>
+
   )
 }
 
